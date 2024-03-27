b0918ae2905d7dc525fc1ea1849c09ed91528fa9
t7bo <tibo7w@gmail.com>
2024-03-27 23:06:39 +0100

    all data scraped from all pages

--- scraping/scraping/spiders/target.py
+++ scraping/scraping/spiders/target.py
@@ -5,31 +5,30 @@ from scrapy_selenium import SeleniumRequest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class TargetSpider(CrawlSpider):
+    
     name = "target"
     allowed_domains = ["www.boxofficemojo.com"]
     URLs = [f"https://www.boxofficemojo.com/weekend/by-year/{year}/?area=FR" for year in range(2021, 2025)]
     
+    
     def start_requests(self):
-        #URLs = [f"https://www.boxofficemojo.com/weekend/by-year/{year}/?area=FR" for year in range(2021, 2025)]
+        # Loop to get every URL sent to next function
         for url in self.URLs:
             yield SeleniumRequest(url=url, callback=self.parse)
         
-        # POUR TESTER SUR UNE SEULE PAGE
+        # Just to test if everything works on one page only
         # yield SeleniumRequest(url='https://www.boxofficemojo.com/weekend/by-year/2024/?area=FR', callback=self.parse)
+
 
     @logger.catch
     def activate_ChromeDriver(self, response):
         
         # Define Options for ChromeDriver
         chrome_options = Options()
-            # 1. Execute Chrome in headless mode (without graphic interface)
+        # Execute Chrome in headless mode (without graphic interface)
         chrome_options.add_argument("--headless")
-            # 2. Desable GPU
-        chrome_options.add_argument("--disable-gpu")
         
         # Activate ChromeDriver
         driver = webdriver.Chrome(options=chrome_options)
@@ -39,16 +38,56 @@ class TargetSpider(CrawlSpider):
         
         return driver
 
+
     @logger.catch
     def parse(self, response):
         
         driver = self.activate_ChromeDriver(response)
         
         weekend_links = driver.find_elements(By.CSS_SELECTOR, 'td.mojo-field-type-date_interval a')
-        
         for link in weekend_links:
-            yield {
+            data = {
                 'year' : ''.join(filter(str.isdigit, response.url)),
                 'url': response.url,
                 'weekend': link.get_attribute('href')
             }
+            yield SeleniumRequest(url=link.get_attribute('href'), callback=self.parse_weekend_page, meta={'link_data': data})
+
+
+    @logger.catch
+    def parse_weekend_page(self, response):
+        
+        link_data = response.meta.get('link_data')
+        
+        driver = self.activate_ChromeDriver(response)
+        
+        titles = []
+        movie_titles = driver.find_elements(By.CSS_SELECTOR, 'tr.mojo-annotation-isNewThisWeek td.mojo-field-type-release a')
+        for title in movie_titles:
+            titles.append(title.text)
+            
+        urls = []
+        movie_urls = driver.find_elements(By.CSS_SELECTOR, 'tr.mojo-annotation-isNewThisWeek td.mojo-field-type-release a')
+        for url in movie_urls:
+            url = urls.append(url.get_attribute('href'))
+        
+        # for 1st week
+        total_grosses = []
+        ttl_grsss = driver.find_elements(By.CSS_SELECTOR, 'tr.mojo-annotation-isNewThisWeek td.mojo-field-type-money')
+        for gross in ttl_grsss:
+            total_grosses.append(gross.text)
+            
+        distributors = []
+        companies = driver.find_elements(By.CSS_SELECTOR, 'tr.mojo-annotation-isNewThisWeek td.mojo-field-type-release_studios a')
+        for distrib in companies:
+            distributors.append(distrib.text)
+        
+        yield {
+            'year': link_data['year'],
+            # 'url': link_data['url'],
+            'weekend': link_data['weekend'],
+            'movie_titles': titles,
+            'movie_urls': urls,
+            'movie_grosses' : total_grosses,
+            'movie_distributors' : distributors,
+        }
