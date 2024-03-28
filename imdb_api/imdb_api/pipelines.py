3fa3217f844a36a7b459ad13bd0645613fe894b2
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-28 05:31:41 +0100

    Implement cleaning pipeline

--- imdb_api/imdb_api/pipelines.py
+++ imdb_api/imdb_api/pipelines.py
@@ -6,8 +6,31 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from loguru import logger
+
+
+TEXT_FIELDS = ("id", "title", "original_title", "genres",
+               "synopsis", "poster_link", "audience")
+INTEGER_FIELDS = ("duration_s", "release_year", "vote_count", "metacritic_score")
 
 
 class ImdbApiPipeline:
+    @logger.catch
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        for field_name in INTEGER_FIELDS:
+            value = adapter.get(field_name)
+            try:
+                adapter[field_name] = int(value)
+            except TypeError:
+                adapter[field_name] = None
+
+        # Clean trailing spaces in textual fields
+        for field_name in TEXT_FIELDS:
+            value = adapter.get(field_name)
+            try:
+                adapter[field_name] = value.strip()
+            except AttributeError:
+                adapter[field_name] = None
         return item
