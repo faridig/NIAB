e4a1e69a3e0a4ad1253ef47927a5172d97c2fc0d
Christian Martiny <christian.martiny@outlook.com>
2024-03-29 16:34:11 +0100

    exemple de requête pour avec les oscarisés devant

--- sql.sql
+++ sql.sql
@@ -1,19 +1,37 @@
- SELECT m.id, m.title, (SELECT ma.person_id
-                  FROM movie_actor ma
-                  JOIN persons p ON p.id = ma.person_id
-                  WHERE ma.movie_id = m.id
-                  ORDER BY p.oscars DESC, ma.star DESC
-                  LIMIT 1) AS start1,
-                  (SELECT ma.person_id
-                  FROM movie_actor ma
-                  JOIN persons p ON p.id = ma.person_id
-                  WHERE ma.movie_id = m.id
-                  ORDER BY p.oscars DESC, ma.star DESC
-                  LIMIT 1 OFFSET 1) AS start2,
-                  (SELECT ma.person_id
-                  FROM movie_actor ma
-                  JOIN persons p ON p.id = ma.person_id
-                  WHERE ma.movie_id = m.id
-                  ORDER BY p.oscars DESC, ma.star DESC
-                  LIMIT 1 OFFSET 2) AS start3
+SELECT m.id, m.title, (SELECT ma.person_id
+                          FROM movie_actor ma
+                          JOIN persons p ON p.id = ma.person_id
+                         WHERE ma.movie_id = m.id
+                      ORDER BY p.oscars DESC, ma.star DESC
+                         LIMIT 1) AS start1,
+                       (SELECT ma.person_id
+                          FROM movie_actor ma
+                          JOIN persons p ON p.id = ma.person_id
+                         WHERE ma.movie_id = m.id
+                      ORDER BY p.oscars DESC, ma.star DESC
+                         LIMIT 1 OFFSET 1) AS start2,
+                       (SELECT ma.person_id
+                          FROM movie_actor ma
+                          JOIN persons p ON p.id = ma.person_id
+                         WHERE ma.movie_id = m.id
+                      ORDER BY p.oscars DESC, ma.star DESC
+                         LIMIT 1 OFFSET 2) AS start3,
+                       (SELECT md.person_id
+                          FROM movie_diretor md
+                          JOIN persons p ON p.id = md.person_id
+                         WHERE md.movie_id = m.id
+                      ORDER BY p.oscars DESC
+                         LIMIT 1) AS director1,
+                       (SELECT md.person_id
+                          FROM movie_diretor md
+                          JOIN persons p ON p.id = md.person_id
+                         WHERE md.movie_id = m.id
+                      ORDER BY p.oscars DESC
+                         LIMIT 1 OFFSET 1) AS director2,
+                       (SELECT md.person_id
+                          FROM movie_diretor md
+                          JOIN persons p ON p.id = md.person_id
+                         WHERE md.movie_id = m.id
+                      ORDER BY p.oscars DESC
+                         LIMIT 1 OFFSET 2) AS director3
  FROM movies m
