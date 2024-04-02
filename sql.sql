d44f05910576efe34ecafe384c16e508c2e0d122
Christian Martiny <christian.martiny@outlook.com>
2024-04-02 15:54:10 +0200

    ajout du SSL

--- sql.sql
+++ sql.sql
@@ -1,37 +1,42 @@
-SELECT m.id, m.title, (SELECT ma.person_id
-                          FROM movie_actor ma
-                          JOIN persons p ON p.id = ma.person_id
-                         WHERE ma.movie_id = m.id
-                      ORDER BY p.oscars DESC, ma.star DESC
-                         LIMIT 1) AS start1,
-                       (SELECT ma.person_id
-                          FROM movie_actor ma
-                          JOIN persons p ON p.id = ma.person_id
-                         WHERE ma.movie_id = m.id
-                      ORDER BY p.oscars DESC, ma.star DESC
-                         LIMIT 1 OFFSET 1) AS start2,
-                       (SELECT ma.person_id
-                          FROM movie_actor ma
-                          JOIN persons p ON p.id = ma.person_id
-                         WHERE ma.movie_id = m.id
-                      ORDER BY p.oscars DESC, ma.star DESC
-                         LIMIT 1 OFFSET 2) AS start3,
-                       (SELECT md.person_id
-                          FROM movie_diretor md
-                          JOIN persons p ON p.id = md.person_id
-                         WHERE md.movie_id = m.id
-                      ORDER BY p.oscars DESC
-                         LIMIT 1) AS director1,
-                       (SELECT md.person_id
-                          FROM movie_diretor md
-                          JOIN persons p ON p.id = md.person_id
-                         WHERE md.movie_id = m.id
-                      ORDER BY p.oscars DESC
-                         LIMIT 1 OFFSET 1) AS director2,
-                       (SELECT md.person_id
-                          FROM movie_diretor md
-                          JOIN persons p ON p.id = md.person_id
-                         WHERE md.movie_id = m.id
-                      ORDER BY p.oscars DESC
-                         LIMIT 1 OFFSET 2) AS director3
- FROM movies m
+ SELECT m.title,
+        m.release_year,
+        m.original_title,
+        (SELECT GROUP_CONCAT(g.genre SEPARATOR ', ')
+	       FROM movie_genre mg
+	       JOIN genre g ON g.id_genre = mg.id_genre
+	      WHERE mg.id_allocine = m.id_allocine
+	   ORDER BY g.genre) AS genres,
+	    m.duration_m,
+	    m.public_rating,
+	    m.vote_count,
+	    m.press_rating,
+	    m.audience,
+	    m.synopsis,
+	    m.poster_link,
+	    (SELECT GROUP_CONCAT(p.name SEPARATOR ', ')
+	           FROM movie_director md
+	           JOIN persons p ON p.id_person = md.id_person
+	          WHERE md.id_allocine = m.id_allocine
+	       ORDER BY p.oscars DESC, p.name) AS directors,
+        (SELECT p.name
+           FROM movie_actor ma
+           JOIN persons p ON p.id_person = ma.id_person
+          WHERE ma.id_allocine = m.id_allocine
+       ORDER BY p.oscars DESC, ma.star DESC, p.name
+          LIMIT 1) AS start1,
+        (SELECT p.name
+           FROM movie_actor ma
+           JOIN persons p ON p.id_person = ma.id_person
+          WHERE ma.id_allocine = m.id_allocine
+       ORDER BY p.oscars DESC, ma.star DESC, p.name
+          LIMIT 1 OFFSET 1) AS start2,
+        (SELECT p.name
+           FROM movie_actor ma
+           JOIN persons p ON p.id_person = ma.id_person
+          WHERE ma.id_allocine = m.id_allocine
+       ORDER BY p.oscars DESC, ma.star DESC, p.name
+          LIMIT 1 OFFSET 2) AS start3,
+        m.youtube_views,
+        m.youtube_likes,
+        m.youtube_comments
+   FROM movies m
