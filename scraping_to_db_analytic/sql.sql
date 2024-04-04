c96476c44f530d89a6006f561293b83939f3bea7
Christian Martiny <christian.martiny@outlook.com>
2024-04-04 15:13:39 +0200

    hydration film

--- scraping_to_db_analytic/sql.sql
+++ scraping_to_db_analytic/sql.sql
@@ -1,9 +1,11 @@
- SELECT m.title,
+CREATE OR REPLACE
+ALGORITHM = UNDEFINED VIEW `db_to_ml` AS
+SELECT m.title,
         m.release_year,
         m.original_title,
         (SELECT GROUP_CONCAT(g.genre SEPARATOR ', ')
           FROM movie_genre mg
-          JOIN genre g ON g.id_genre = mg.id_genre
+          JOIN genres g ON g.id_genre = mg.id_genre
          WHERE mg.id_allocine = m.id_allocine
       ORDER BY g.genre) AS genres,
         m.duration_m,
@@ -22,22 +24,22 @@
            FROM movie_actor ma
            JOIN persons p ON p.id_person = ma.id_person
           WHERE ma.id_allocine = m.id_allocine
-       ORDER BY p.oscars DESC, ma.star DESC, p.name
+       ORDER BY p.oscars DESC, p.name
           LIMIT 1) AS start1,
         (SELECT p.name
            FROM movie_actor ma
            JOIN persons p ON p.id_person = ma.id_person
           WHERE ma.id_allocine = m.id_allocine
-       ORDER BY p.oscars DESC, ma.star DESC, p.name
+       ORDER BY p.oscars DESC, p.name
           LIMIT 1 OFFSET 1) AS start2,
         (SELECT p.name
            FROM movie_actor ma
            JOIN persons p ON p.id_person = ma.id_person
           WHERE ma.id_allocine = m.id_allocine
-       ORDER BY p.oscars DESC, ma.star DESC, p.name
+       ORDER BY p.oscars DESC, p.name
           LIMIT 1 OFFSET 2) AS start3,
         m.youtube_views,
         m.youtube_likes,
         m.youtube_comments
    FROM movies m
-   
+   ;
