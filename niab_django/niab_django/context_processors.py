fdbf793a1f362231647b56101a409bd9833fe4d0
Christian Martiny <christian.martiny@outlook.com>
2024-04-09 16:38:30 +0200

    début de la charte graphique

---
+++ niab_django/niab_django/context_processors.py
@@ -0,0 +1,5 @@
+def global_variables(request):
+    return {
+        'company_name': 'Chris-Farid-Greg-T7bo corporation ™',
+        'company_name_small': 'CFGT™',
+    }
--- niab_django/niab_django/settings/base.py
+++ niab_django/niab_django/settings/base.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'niab_django.context_processors.global_variables',
             ],
         },
     },
@@ -133,7 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
