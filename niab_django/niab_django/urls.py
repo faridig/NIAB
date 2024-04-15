62fa5beb2af37a57724d64035942ed24e494add9
Christian Martiny <christian.martiny@outlook.com>
2024-04-15 16:15:54 +0200

    c'est le commit du lundi à 16:15

--- niab_django/niab_django/urls.py
+++ niab_django/niab_django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+
+handler404 = 'app_main.views.error_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
