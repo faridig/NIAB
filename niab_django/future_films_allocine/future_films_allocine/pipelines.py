16f4dbb6923cae29cdcb0d088ec6b58d0d9eb061
GMartin-Data <gregory.martin.data@gmail.com>
2024-04-16 11:45:35 +0200

    Implement IncomingToMySQLPipeline (with an import issue though...)

--- niab_django/future_films_allocine/future_films_allocine/pipelines.py
+++ niab_django/future_films_allocine/future_films_allocine/pipelines.py
@@ -7,8 +7,10 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from loguru import logger
+import mysql.connector
 
 from .utils import convert_dates, convert_duration
+import dot_env
 
 
 LIST_FIELDS = ("casting", "director", "genres", "nationality")
@@ -53,3 +55,60 @@ class CleanPipeline:
             adapter["budget"] = None
    
         return item
+
+
+class IncomingToMySQLPipeline:
+    def __init__(self):
+        # Connect to BDD
+        print()
+        print(">>>>>>>>>>>INIT INCOMING MOVIES<<<<<<<<<<<<<<<")
+        self.conn = mysql.connector.connect(
+            host = dot_env.FUNCTIONAL_HOST,
+            user = dot_env.FUNCTIONAL_USER,
+            password = dot_env.FUNCTIONAL_PASSWORD,
+            database = dot_env.FUNCTIONAL_DATABASE,
+            ssl_ca=dot_env.FUNCTIONAL_SSL
+        )
+
+        self.cur = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        print()
+        print(">>>>>>>>>>>INSERT INCOMING MOVIES<<<<<<<<<<<<<<<")
+        try:
+            id_allocine = item["film_id"]
+            title = item["title"]
+            img_src = item["img_src"]
+            release_date = item["release"]
+            duration = item["duration"]
+            pivot_genres = item["genres"]
+            synopsis = item["synopsis"]
+            nationality = item["nationality"]
+            distributor = item["distributor"]
+            budget = item["budget"]
+            pivot_director = item["director"]
+            pivot_casting = item["casting"]
+            copies = item["copies"]
+            entries = item["entries"]
+
+            request = f"""
+            INSERT INTO movies_w0 (
+                id_allocine, title, img_src, release_date, duration,
+                pivot_genres, synopsis, nationality, distributor,
+                budget, pivot_director, pivot_casting, copies, entries
+                )
+            VALUES ({id_allocine}, {title}, {img_src}, {release_date}, {duration},
+                    {pivot_genres}, {synopsis}, {nationality}, {distributor},
+                    {budget}, {pivot_director}, {pivot_casting}, {copies}, {entries})
+            """
+            self.cur.execute(request)
+        except BaseException as e:
+            print(e)
+            print("request =", request)
+
+        self.conn.commit()
+        return item
+    
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
